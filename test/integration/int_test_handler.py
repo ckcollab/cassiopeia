4b73390994f97038ee5fb0fbf8528dfd8d8a21a7
Eric Carmichael <eric@ckcollab.com>
2015-12-27 15:05:12 -0800

    rate limit env var option

--- test/integration/int_test_handler.py
+++ test/integration/int_test_handler.py
@@ -5,12 +5,23 @@ from cassiopeia.type.core.common import LoadPolicy
 
 
 RIOT_API_KEY = os.environ.get("RIOT_API_KEY")
-
 if RIOT_API_KEY:
     riotapi.set_api_key(os.environ["RIOT_API_KEY"])
     riotapi.set_region("NA")
     riotapi.print_calls(True)
     riotapi.set_load_policy(LoadPolicy.lazy)
+
+RIOT_API_KEY_LIMITS = os.environ.get("RIOT_API_KEY_LIMITS")
+if RIOT_API_KEY_LIMITS:
+    # Expected ENV var format is something like:
+    # RIOT_API_KEY_LIMITS=1500/10,90000/600
+    rates = RIOT_API_KEY_LIMITS.replace(" ", "")
+    rate_tuples = []
+    for rate in rates.split(","):
+        calls, time = rate.split("/")
+        rate_tuples.append((int(calls), int(time)))
+    riotapi.set_rate_limits(*rate_tuples)
+
 
 champion_id = 35
 champion_name = "Thresh"
